1ffd4a9ce3dbb9ddeb561704805036eba17b9b32
Fred K. Schott <fschott@google.com>
2018-03-22 11:38:19 -0500

    auto-converted by polymer-modulizer

--- test/attr-for-selected-elements.js
+++ test/attr-for-selected-elements.js
@@ -1,4 +1,7 @@
-<!--
+import '@polymer/polymer/polymer-legacy.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+/**
 @license
 Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,25 +9,19 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-
-<link rel="import" href="../../polymer/polymer.html">
-
-<dom-module id="attr-reflector">
-  <template>
+*/
+Polymer({
+  _template: html`
     <div>{{someAttr}}</div>
-  </template>
-  <script>
-    Polymer({
-      is: 'attr-reflector',
+`,
 
-      properties: {
-        someAttr: {
-          type: String,
-          value: "",
-          reflectToAttribute: true
-        }
-      }
-    });
-  </script>
-</dom-module>
+  is: 'attr-reflector',
+
+  properties: {
+    someAttr: {
+      type: String,
+      value: "",
+      reflectToAttribute: true
+    }
+  }
+});
